a3485ff8611bbf4aee4e79cda1c440be7b0017c1
Paulo-Teixeira-Delgado <113623874+Paulo-Teixeira-Delgado@users.noreply.github.com>
2022-09-29 18:22:59 +0200

    Update cosillas.java

--- cosillas.java
+++ cosillas.java
@@ -1,8 +1,6 @@
 class cosillas {
     public static void main(String[] args) {
-        
-        int i = 2, j = 1;
-        char c = 'a';
+    
         ((Object) (i+c)).getClass().getSimpleName();
     }
 }
