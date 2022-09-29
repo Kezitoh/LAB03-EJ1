e32e74875614795349b93c0325d834b090270c95
Kezitoh <alejandrodebduq@gmail.com>
2022-09-29 18:21:24 +0200

    Create cosillas.java

---
+++ cosillas.java
@@ -0,0 +1,8 @@
+class cosillas {
+    public static void main(String[] args) {
+        
+        int i = 2, j = 1;
+        char c = 'a';
+        ((Object) (i+c)).getClass().getSimpleName();
+    }
+}
